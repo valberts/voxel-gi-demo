a82cb5c19530ba045c584a00b94d38a0214c4bcc
valberts <albertsson.vincent@gmail.com>
2024-03-21 19:49:16 +0100

    Draw triangle in window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,214 @@
-use voxel_gi_demo::run;
+use backend::pipeline_builder::PipelineBuilder;
+mod backend;
 
+use std::time;
+use winit::{
+    dpi::PhysicalSize,
+    event::*,
+    event_loop::EventLoop,
+    event_loop::EventLoopBuilder,
+    event_loop::EventLoopProxy,
+    keyboard::{KeyCode, PhysicalKey},
+    window::{Window, WindowBuilder},
+};
+
+struct State<'a> {
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: PhysicalSize<u32>,
+    window: &'a Window,
+    render_pipeline: wgpu::RenderPipeline,
+}
+
+impl<'a> State<'a> {
+    async fn new(window: &'a Window) -> State<'a> {
+        let size = window.inner_size();
+
+        let instance_descriptor = wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            ..Default::default()
+        };
+        let instance = wgpu::Instance::new(instance_descriptor);
+        let surface = instance.create_surface(window).unwrap();
+
+        let adapter_descriptor = wgpu::RequestAdapterOptionsBase {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        };
+        let adapter = instance.request_adapter(&adapter_descriptor).await.unwrap();
+
+        let device_descriptor = wgpu::DeviceDescriptor {
+            required_features: wgpu::Features::empty(),
+            required_limits: wgpu::Limits::default(),
+            label: Some("Device"),
+        };
+        let (device, queue) = adapter
+            .request_device(&device_descriptor, None)
+            .await
+            .unwrap();
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+        let surface_format = surface_capabilities
+            .formats
+            .iter()
+            .copied()
+            .filter(|f| f.is_srgb())
+            .next()
+            .unwrap_or(surface_capabilities.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+        surface.configure(&device, &config);
+
+        let mut pipeline_builder = PipelineBuilder::new();
+        pipeline_builder.set_shader_module("shaders/shader.wgsl", "vs_main", "fs_main");
+        pipeline_builder.set_pixel_format(config.format);
+        let render_pipeline = pipeline_builder.build_pipeline(&device);
+
+        Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            render_pipeline,
+        }
+    }
+
+    fn resize(&mut self, new_size: PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let drawable = self.surface.get_current_texture()?;
+        let image_view_descriptor = wgpu::TextureViewDescriptor::default();
+        let image_view = drawable.texture.create_view(&image_view_descriptor);
+
+        let command_encoder_descriptor = wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        };
+        let mut command_encoder = self
+            .device
+            .create_command_encoder(&command_encoder_descriptor);
+
+        let color_attachment = wgpu::RenderPassColorAttachment {
+            view: &image_view,
+            resolve_target: None,
+            ops: wgpu::Operations {
+                load: wgpu::LoadOp::Clear(wgpu::Color {
+                    r: 0.75,
+                    g: 0.5,
+                    b: 0.25,
+                    a: 1.0,
+                }),
+                store: wgpu::StoreOp::Store,
+            },
+        };
+
+        let render_pass_descriptor = wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(color_attachment)],
+            depth_stencil_attachment: None,
+            occlusion_query_set: None,
+            timestamp_writes: None,
+        };
+
+        {
+            let mut render_pass = command_encoder.begin_render_pass(&render_pass_descriptor);
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
+        }
+
+        self.queue.submit(std::iter::once(command_encoder.finish()));
+
+        drawable.present();
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum CustomEvent {
+    Timer,
+}
+
+async fn run() {
+    env_logger::init();
+    let mut key_table = vec![false; 255].into_boxed_slice();
+
+    let event_loop: EventLoop<CustomEvent> = EventLoopBuilder::<CustomEvent>::with_user_event()
+        .build()
+        .unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let event_loop_proxy: EventLoopProxy<CustomEvent> = event_loop.create_proxy();
+
+    std::thread::spawn(move || loop {
+        std::thread::sleep(time::Duration::from_millis(17));
+        event_loop_proxy.send_event(CustomEvent::Timer).ok();
+    });
+
+    let mut state = State::new(&window).await;
+
+    event_loop
+        .run(move |event, elwt| match event {
+            Event::UserEvent(..) => {
+                state.window.request_redraw();
+            }
+            Event::WindowEvent { window_id, event } if window_id == state.window.id() => {
+                match event {
+                    WindowEvent::Resized(physical_size) => state.resize(physical_size),
+                    WindowEvent::CloseRequested => {
+                        println!("The close button was pressed; stopping");
+                        elwt.exit();
+                    }
+                    WindowEvent::KeyboardInput { event, .. } => {
+                        if let PhysicalKey::Code(code) = event.physical_key {
+                            key_table[code as usize] = event.state.is_pressed();
+                        }
+                    }
+                    WindowEvent::RedrawRequested => match state.render() {
+                        Ok(_) => {}
+                        Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                        Err(wgpu::SurfaceError::OutOfMemory) => elwt.exit(),
+                        Err(e) => eprintln!("{:?}", e),
+                    },
+                    _ => {}
+                }
+            }
+            Event::AboutToWait => {
+                // Application update code.
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw in
+                // applications which do not always need to. Applications that redraw continuously
+                // can render here instead.
+                if key_table[KeyCode::Escape as usize] {
+                    println!("Escape key was pressed; stopping");
+                    elwt.exit();
+                }
+                state.window.request_redraw();
+            }
+            _ => {}
+        })
+        .expect("Error!");
+}
 fn main() {
-    run();
+    pollster::block_on(run());
 }
